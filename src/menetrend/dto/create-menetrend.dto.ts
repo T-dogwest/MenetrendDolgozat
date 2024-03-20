4c864ac10bb25ed014dd412212763ccdf4793e78
Csontos <csontostibor48@gmail.com>
2024-03-20 13:55:08 +0100

    Done2

--- src/menetrend/dto/create-menetrend.dto.ts
+++ src/menetrend/dto/create-menetrend.dto.ts
@@ -1,12 +1,12 @@
 import { IsNotEmpty } from "class-validator";
 
 export class CreateMenetrendDto {
-    @IsNotEmpty()
+    @IsNotEmpty({message:"Nem lehet üres az indulási hely"})
     from: string;
-    @IsNotEmpty()
+    @IsNotEmpty({message:"Nem lehet üres az érkezési hely"})
     to: string;
-    @IsNotEmpty()
+    @IsNotEmpty({message:"Nem lehet üres a menetidő"})
     time: string;
-    @IsNotEmpty()
+    @IsNotEmpty({message:"Nem lehet üres az indulási idő"})
     start:string;
 }
